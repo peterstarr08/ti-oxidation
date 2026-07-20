ea06ab178320329e12b95cddde1d010a18b71b3e
Rubin Singh <singhrubin08@gmail.com>
2026-07-20 13:48:31 +0530

    [box search] box search init

--- src/ti_oxidation/io/lammps.py
+++ src/ti_oxidation/io/lammps.py
@@ -19,16 +19,24 @@ class LAMMPS_Input:
         inp = cls(read_file(path))
         return inp
 
+    def find_variable(self, var_list):
+        regEx = re.findall(rf"^variable\s*[{"".join(var_list)}]\s*equal\s.*\n", self.content, re.MULTILINE)
+        content = [( item.split()[1], float(item.split()[-1])) for item in regEx]
+        if len(content)==0:
+            print(f"Warning: No results for {var_list} ")
+        return content 
+
+
     def update_variable(self, var_name, value):
         regEx = fr'variable\s*{var_name}\s*equal\s*.*\n'
-        print(f"Variable found {regEx}")
+        #print(f"Variable found {regEx}")
         value = f'variable {var_name} equal {value}\n'
-        print(f"Replacing with {value}")
+        #print(f"Replacing with {value}")
 
         return LAMMPS_Input(re.sub(regEx, value, self.content))
 
 
-
+# For backward compatibility
 def lammps_log_extract(path, keys):
     keys = set(keys)
     result = {k: None for k in keys}
@@ -45,3 +53,43 @@ def lammps_log_extract(path, keys):
                 result[key] = value.strip()
 
     return result
+
+
+def parse_energy_minim(lines):
+    # print(lines)
+    keys = {}
+    for i, line in enumerate(lines):
+        if "Stopping criterion" in line:
+            keys['stop'] = line.split('=')[-1].strip()
+
+        if "Energy initial, next-to-last, final =" in line:
+            energy_vals = lines[i+1].strip().split()
+            keys['ener_init'] = float(energy_vals[0])
+            keys['ener_penult'] = float(energy_vals[1])
+            keys['ener_final'] = float(energy_vals[2])
+
+            keys['dE'] = keys['ener_final'] - keys['ener_penult']
+        if "Force max component" in line:
+            keys['force_max_comp'] = float(line.split('=')[-1].strip().split()[-1])
+
+    return keys
+
+
+class LAMMPS_Log:
+    def __init__(self, path):
+        self.path = path
+        with open(path) as f:
+            lines = f.readlines()
+        self.variables = {}
+        for i, line in enumerate(lines):
+            # Check for minim block
+            if 'Minimization stats:' in line:
+                self.minim_stat = parse_energy_minim(lines[i:i+8])
+
+            # Potential variables
+            if '=' in line:
+                strip_search = line.split('=')
+                strip_var = strip_search[0].strip()
+                if not ' ' in strip_var:
+                    strip_value = strip_search[-1].strip()
+                    self.variables[strip_var] = strip_value
---
+++ src/ti_oxidation/lattice_param_pe_surface.py
@@ -0,0 +1,64 @@
+import argparse
+import re
+from pathlib import Path
+import numpy as np
+
+from ti_oxidation.io.lammps import *
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description="Lattice Parameter Calculator using Box Method")
+
+    parser.add_argument("path")
+    parser.add_argument("--range", type=float, default=0.03)
+    parser.add_argument("--steps", type=float, default=0.005)
+    parser.add_argument("--lattice", nargs='+', choices=['a', 'b', 'c'])
+    parser.add_argument("--ftol", type=float, required=True, default=0.025)
+    parser.add_argument("--out-dir", default='./box_lattice_calc')
+
+    return parser.parse_args()
+
+
+def generate_input(out_dir, params, template):
+    out_dir = Path(out_dir).resolve()
+    param1, param2 = params
+    out_path = out_dir / f'{param1[0]}_{param1[1]}_{param2[0]}_{param2[1]}'
+    out_path.mkdir(parents=True, exist_ok=True)
+
+    temp = template.update_variable(param1[0], param1[1])
+    temp = temp.update_variable(param2[0], param2[1]) 
+
+    with open(out_path/'lammps.in', 'w') as f:
+        f.write(temp.content)
+
+
+def main():
+    args = arg_parse()
+    
+    if len(args.lattice) != 2:
+        raise RuntimeError(f"Only 2d box search supported. Give {len(args.lattice)}")
+
+    template = LAMMPS_Input.from_file(args.path)
+
+    lattice_param = template.find_variable(args.lattice)
+
+    ranges = []
+
+    for param, value in lattice_param:
+        lx = value*(1-args.range)
+        hx = value*(1+args.range)
+        steps = value*args.steps
+        gen_range = np.arange(lx, hx, steps)
+        ranges.append((param ,gen_range))
+        print(f'{param}\t{value}\tlx {lx} hx {hx}\t steps {steps}\t len {len(gen_range)}')
+    
+    for p1_v in ranges[0][1]:
+        for p2_v in ranges[1][1]:
+            new_lattice = []
+            new_lattice.append((ranges[0][0], p1_v))
+            new_lattice.append((ranges[1][0], p2_v))
+            generate_input(args.out_dir, new_lattice, template)
+
+if __name__=="__main__":
+    main()
+
+
